e963fc9b11b7c35949e74374355098a0086e2a6b
bunniicat <nina.nny97@gmail.com>
2019-12-10 14:25:20 +0100

    sadwsd

--- GOT_code/character.js
+++ GOT_code/character.js
@@ -4,15 +4,26 @@ function getInfo(id) {
         .then(charInfo => populateCard(charInfo));
 }
 
+
+
 const popup = document.getElementById("popup");
 
 function populateCard(character) {
     popup.style.display = "block";
+    const getName = character.name.replace(/\s/g, '');
+    let getBio = getNameBio;
+    getBio = getBio;
+    console.log(getBio);
     document.getElementById("popupInfo").innerHTML = "";
     document.getElementById("popupInfo").innerHTML += `<div class="character-info">
     <p>Character Name: ${character.name}</p>
     <p>Gender: ${character.gender}</p>
+    <p>Born: ${character.born}</p>
     <p>Title: ${character.titles}</p>
+    <br>
+    <p class="character-bio">BIOGRAPHY:</p>
+    <p class="character-bio-text">${getBio}</p>
+    <button id="character-btn" onclick="selectChar()" data-select="${getName}">xxxxx</button>
     </div>`
 }
 
@@ -22,20 +33,18 @@ window.onclick = function (event) {
     }
 }
 
-for (i = 1; i < 11; i++) {
-    let button = document.getElementById(`btn${i}`);
-    button.onclick = saveTokens;
+const PetyrBaelishbio = "I'm probably in love with your mum, but I will make you queen."
+const CerseiLannisterbio = "Swipe right or die."
+const DaenerysTargaryenbio = "Come find out why they call me mad queen."
+const SansaStarkbio = "Been married a few times but nothing serious, not looking for another little finger in my life."
+const Drogobio = "The dothraki have no word for hair cut."
+const GreyWormbio = "Saw the love of my life get executed, not looking for anything serious atm."
+const Melisandrebio = "Sometimes I light people on fire."
+const AryaStarkbio = "A girl has no preference."
+const MargaeryTyrellbio = "My best relationship was with a gay man." 
 
-    function saveTokens() {
-        if ("playerToken1" in sessionStorage) {
-            sessionStorage.setItem("playerToken2", this.getAttribute("data-image"));
-            sessionStorage.setItem("playerName2", this.getAttribute("data-name"));
-            if ("playerToken1" in sessionStorage && "playerToken2" in sessionStorage) {
-                document.getElementById("instructions").innerHTML = `<a href="board.html"><button class="start-btn">Start Game</button></a>`
-            }
-        } else {
-            sessionStorage.setItem("playerToken1", this.getAttribute("data-image"));
-            sessionStorage.setItem("playerName1", this.getAttribute("data-name"));
-        }
-    }
+function selectChar() {
+    const btn = document.querySelector("#character-btn");
+    theData = btn.getAttribute("data-select");
+    sessionStorage.setItem("yourMatch", theData);
 }
